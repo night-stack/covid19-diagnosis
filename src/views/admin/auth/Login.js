7c4c9f39a9735e6cd3c51cffed7da97e592cc143
agung <agunghandoko112@gmail.com>
2021-07-25 21:25:23 +0700

    fix bugs

--- src/views/admin/auth/Login.js
+++ src/views/admin/auth/Login.js
@@ -148,13 +148,12 @@ export default function Login() {
             </div>
             <div className="flex flex-wrap mt-6 relative">
               <div className="w-1/2">
-                <a
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
+                <Link
+                  to="/auth/admin/forgot-password"
                   className="text-blueGray-200"
                 >
                   <small>Forgot password?</small>
-                </a>
+                </Link>
               </div>
               <div className="w-1/2 text-right">
                 <Link to="/auth/admin/register" className="text-blueGray-200">
