d615f346750715a828e739207702dc2ce72971bf
agung <agunghandoko112@gmail.com>
2021-07-26 16:17:06 +0700

    fix error

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -97,18 +97,17 @@ export default function Navbar(props) {
                   </li>
                 )}
 
-              {!user ||
-                (user && user.role !== "user" && (
-                  <li className="flex items-center">
-                    <button
-                      className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={login}
-                    >
-                      <i className="fas fa-sign-in-alt"></i> Login
-                    </button>
-                  </li>
-                ))}
+              {!user && (
+                <li className="flex items-center">
+                  <button
+                    className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={login}
+                  >
+                    <i className="fas fa-sign-in-alt"></i> Login
+                  </button>
+                </li>
+              )}
 
               {user &&
                 user.role === "user" &&
